e804050ca7d4ce8ab7f1d1f8292083bd0bf12924
Thomas Reto Strub <ths@strubt.ch>
2015-10-19 15:20:50 +0200

    add syntax examples

--- src/ch/bfh/cssem/lambdaexpressions/SampleClass.java
+++ src/ch/bfh/cssem/lambdaexpressions/SampleClass.java
@@ -1,22 +1,24 @@
 package ch.bfh.cssem.lambdaexpressions;
 
-public class SampleClass {
+public final class SampleClass {
 
-	public static void main(String[] args) {
+	private SampleClass() {}
+
+	public static void main(String... args) {
 
 		SampleInterface.staticFunction();
 
-		myFunction(new SampleInterface() { // anonymous class (pre-Java 8)
+		SampleClass.myFunction(new SampleInterface() { // anonymous class (pre-Java 8)
 			@Override
 			public void abstractFunction() {
 				System.out.println("Hello, World! by anonymous class");
 			}
 		});
 
-		myFunction(() -> System.out.println("Hello, World! by lambda expression")); // lambda expression (Java 8)
+		SampleClass.myFunction(() -> System.out.println("Hello, World! by lambda expression")); // lambda expression (Java 8)
 	}
 
-	public static void myFunction(SampleInterface sample) {
+	private static void myFunction(SampleInterface sample) {
 
 		sample.abstractFunction();
 		sample.defaultFunction();
---
+++ src/ch/bfh/cssem/lambdaexpressions/SyntaxClass.java
@@ -0,0 +1,60 @@
+package ch.bfh.cssem.lambdaexpressions;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public final class SyntaxClass {
+
+	private SyntaxClass() {}
+
+	public static void main(String... args) {
+
+		SyntaxClass.println(() -> String.format("Hello, World! at %s", LocalDateTime.now()));
+
+		List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+		List<Integer> even1 = SyntaxClass.even(numbers, n -> n % 2 == 0);
+		List<Integer> even2 = SyntaxClass.even(numbers, n -> n % 2 == 0);
+
+		System.out.println(even1);
+		System.out.println(even2);
+
+		String separator = " ";
+		List<String> elements = Arrays.asList("Hello,", "World!", "by", "strut1", "and", "touwm1");
+		String joined1 = SyntaxClass.join(elements, (a, b) -> a + separator + b);
+		String joined2 = SyntaxClass.join(elements, SyntaxClass::join);
+
+		System.out.println(joined1);
+		System.out.println(joined2);
+	}
+
+	private static void println(NullaryInterface<String> message) {
+
+		System.out.println(message.execute());
+	}
+
+	private static List<Integer> even(List<Integer> numbers, UnaryInterface<Integer, Boolean> predicate) {
+
+		List<Integer> even = new ArrayList<>();
+		for (Integer number : numbers)
+			if (predicate.execute(number))
+				even.add(number);
+
+		return even;
+	}
+
+	private static String join(String string1, String string2) {
+
+		return String.format("%s %s", string1, string2);
+	}
+
+	private static String join(List<String> elements, BinaryInterface<String, String, String> joiner) {
+
+		String joined = !elements.isEmpty() ? elements.get(0) : null;
+		for (int i = 1; i < elements.size(); i++)
+			joined = joiner.execute(joined, elements.get(i));
+
+		return joined;
+	}
+}
