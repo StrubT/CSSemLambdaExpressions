7efa8c94cefc0e13c898e8a7425abb4e8012fc93
Marc <marc.touw@gmail.com>
2015-10-20 11:32:26 +0200

    Created Collections class

---
+++ src/ch/bfh/cssem/lambdaexpressions/CollectionsClass.java
@@ -0,0 +1,16 @@
+package ch.bfh.cssem.lambdaexpressions;
+
+import java.util.Collection;
+
+/**
+ * Created by Marc on 20.10.2015.
+ */
+public class CollectionsClass {
+
+  public static void main (String []args) {
+
+    Collection
+
+  }
+
+}
