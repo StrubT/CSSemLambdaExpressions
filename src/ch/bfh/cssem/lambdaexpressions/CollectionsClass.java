2727efd7c45d9d23d4ead7c1db5555a52f1fbe05
Marc <marc.touw@gmail.com>
2015-10-21 09:48:49 +0200

    Added examples in CollectionsClass

--- src/ch/bfh/cssem/lambdaexpressions/CollectionsClass.java
+++ src/ch/bfh/cssem/lambdaexpressions/CollectionsClass.java
@@ -9,7 +9,7 @@ public class CollectionsClass {
 
   public static void main (String []args) {
 
-    List<Integer> myColl = new ArrayList<>(Arrays.asList(1, 3, 52, 47, 8, 2, 15, 38, 15, 7, 42, 31, 43, 5));
+    List<Integer> myColl = Arrays.asList(1, 3, 52, 47, 8, 2, 15, 38, 15, 7, 42, 31, 43, 5);
     myColl.removeIf(number -> number > 20);
 
     for (Integer number : myColl) {
@@ -18,5 +18,4 @@ public class CollectionsClass {
 
     myColl.forEach((number) -> System.out.println("By lambda expression: " + number));
   }
-
 }
