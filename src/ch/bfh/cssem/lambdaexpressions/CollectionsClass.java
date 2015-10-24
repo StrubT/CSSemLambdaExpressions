fc9dba19726e08e78855670400dd791c1f9ca228
Thomas Reto Strub <ths@strubt.ch>
2015-10-24 18:14:54 +0200

    collections

--- src/ch/bfh/cssem/lambdaexpressions/CollectionsClass.java
+++ src/ch/bfh/cssem/lambdaexpressions/CollectionsClass.java
@@ -1,19 +1,38 @@
 package ch.bfh.cssem.lambdaexpressions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-
+import java.util.stream.Collectors;
 
 public class CollectionsClass {
 
-  public static void main (String []args) {
+	public static void main(String[] args) {
 
-    Collection<Integer> myColl = Arrays.asList(1, 3, 52, 47, 8, 2, 15, 38, 15, 7, 42, 31, 43, 5);
-    myColl.removeIf(number -> number > 20);
+		Collection<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, 52, 47, 8, 2, 15, 38, 15, 7, 42, 31, 43, 5));
+		System.out.println(String.join(", ", numbers.stream().map(Object::toString).collect(Collectors.toList())));
 
-    for (Integer number : myColl)
-      System.out.println("By for loop: " + number);
+		numbers.removeIf(n -> n > 20);
+		System.out.println(String.join(", ", numbers.stream().map(Object::toString).collect(Collectors.toList())));
 
-    myColl.forEach(number -> System.out.println("By lambda expresion: " + number));
-  }
+		Iterable<String> students = Arrays.asList("strut1", "touwm1");
+		for (String student : students)
+			System.out.printf("By for loop: %s%n", student);
+
+		students.forEach(n -> System.out.printf("By lambda expression: %s%n", n));
+
+		int pageSize = 3;
+		int pageIndex = 1;
+		Collection<String> names = Arrays.asList("Heinrich", "Joschua", "Hans", "Peter", "Hella", "Marc", "Hagrid", "Erich", "Heinz", "Thomas", "Heidi", "Marianne", "Heinrich", "Janick", "Heinz", "Reto", "Hermione");
+		Collection<String> namesH = names.stream()
+																		 .parallel()
+																		 .distinct()
+																		 .map(String::toUpperCase)
+																		 .filter(l -> l.startsWith("H"))
+																		 .skip(pageIndex * pageSize).limit(pageSize)
+																		 .collect(Collectors.toList());
+
+		System.out.println(String.join(", ", names));
+		System.out.println(String.join(", ", namesH));
+	}
 }
